bb5c9ab51cff1cac0205945d1683535f2a752d60
JOAO PAULO PEREIRA DE MEDEIROS <joaopaulopmedeiros@gmail.com>
2023-10-18 17:04:58 -0300

    feat: custom vu's and duration

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM grafana/k6
 
 COPY src/script.js src/script.js
 
-CMD ["run", "--vus", "10", "--duration", "30s", "src/script.js"]
+CMD ["run", "src/script.js"]
--- src/script.js
+++ src/script.js
@@ -1,6 +1,14 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+const VUS = parseInt(__ENV.K6_VUS || '10', 10);
+const DURATION = __ENV.K6_DURATION || '30s';
+
+export const options = {
+    vus: VUS,
+    duration: DURATION,
+  };
+
 export default function () {
   const res = http.get('https://httpbin.test.k6.io/');
   check(res, { 'status was 200': (r) => r.status == 200 });
